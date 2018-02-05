a6abb3e2337a8a0ba2d5d2d31e7e77e94a6c67f1
LiUhoNgDan <814074992@qq.com>
2018-02-05 15:51:37 +0800

    update account

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['account.component.scss']
+  template: '<router-outlet></router-outlet>'
 })
 export class AccountComponent implements OnInit {
 
